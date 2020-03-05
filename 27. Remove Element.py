e6e49a34239e2a1767638b7075bb76b9a1f93f47
Thaiph1308 <ryuamakusa123@gmail.com>
2020-03-05 22:30:25 +0700

    Temp commit
    Insert pos need to be complete

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,14 +1,22 @@
 nums = [0,1,2,2,2,2,2,3,0,4,2]
 val = 2
 
-def remove_elements(nums,val):
+# def remove_elements(nums,val):
+#     i = 0
+#     while (i < len(nums)):
+#         if (nums[i] == val):
+#             print(nums,i,val)
+#             nums.pop(i)
+#         else:
+#             i = i + 1
+#     print(len(nums))
+#     return len(nums)
+def remove_elements(nums, val):
     i = 0
-    while (i < len(nums)):
-        if (nums[i] == val):
-            print(nums,i,val)
-            nums.pop(i)
-        else:
-            i = i + 1
-    print(len(nums))
-    return len(nums)
-remove_elements(nums,val)
+    for x in nums:
+        if x != val:
+            nums[i] = x
+            i += 1
+    print(nums)
+    return i
+print(remove_elements(nums,val))
--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,22 +1,36 @@
-nums = [1,3,5,6]
-target = 0
-def searchInsert(nums,target):
-    '''
-    O(n) Approach
-    '''
-    lower , greater = -1 , -1 
-    for i in range(len(nums)):
-        if nums[i] == target:
-            return i
-        else:
-            if nums[i] < target:
-                lower = i
-            if nums[i] > target:
-                greater = i
-        print(lower,greater,sep=" | ")
-    return 0 if lower == -1 else lower + 1
+nums = [1,2,3,4,5,6,7,8,9,10,11,14,16,18,19,22,31,38,44,66,99]
+target = 14
+# def searchInsert(nums,target):
+#     '''
+#     O(n) Approach
+#     '''
+#     lower , greater = -1 , -1 
+#     for i in range(len(nums)):
+#         if nums[i] == target:
+#             return i
+#         else:
+#             if nums[i] < target:
+#                 lower = i
+#             if nums[i] > target:
+#                 greater = i
+#         print(lower,greater,sep=" | ")
+#     return 0 if lower == -1 else lower + 1
 
-'''
-O(nlogn) Approach, mid = greater + lower /2
-'''
+# '''
+# O(logn) Approach, mid = greater + lower /2
+# '''
+def searchInsert(nums,target):
+    low = 0
+    high = len(nums)-1
+    mid = int((low + high)/2)
+    i = 0
+    while i < len(nums):
+        if (nums[mid] == target):
+            return True
+        if (nums[mid] < target):
+            high = int((mid + high)/2) 
+        if (nums[mid] > target):
+            low = int((low+mid)/2) , low
+        i = i+1
+
 print(searchInsert(nums,target))
